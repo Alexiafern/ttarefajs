a630ae4d5c9afcc3252d27e1ae705c006754a8ba
Alexiafern <alexiafernanda718@gmail.com>
2023-10-17 09:45:20 -0300

    Add arquivos

---
+++ sen.js
@@ -0,0 +1,16 @@
+
+var nome = "Fernanda"; 
+var idade = 17;  
+
+
+var mensagem = "Olá, meu nome é " + nome + " e eu tenho " + idade + " anos.";
+
+console.log(mensagem);
+
+var numero1 = 10;
+var numero2 = 5;
+var resultado = numero1 + numero2;
+
+console.log("A soma de " + numero1 + " e " + numero2 + " é igual a " + resultado);
+console.log(typeof nome);
+console.log(typeof idade);
